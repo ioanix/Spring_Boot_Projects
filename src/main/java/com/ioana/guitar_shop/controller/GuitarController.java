1ff4dedbb00680732e7df1bc27031d11abf0f147
ioanix <ioana_1001@yahoo.com>
2020-08-16 15:51:56 +0300

    error try

--- src/main/java/com/ioana/guitar_shop/controller/GuitarController.java
+++ src/main/java/com/ioana/guitar_shop/controller/GuitarController.java
@@ -3,10 +3,8 @@ package com.ioana.guitar_shop.controller;
 import com.ioana.guitar_shop.model.Guitar;
 import com.ioana.guitar_shop.service.GuitarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -29,15 +27,8 @@ public class GuitarController {
     @ResponseBody
     public Guitar getGuitarById(@PathVariable("serialNumber") String serialNumber) {
 
-        Guitar returnedGuitar =  guitarService.getGuitarById(serialNumber);
+        return guitarService.getGuitarById(serialNumber);
 
-        if(returnedGuitar.equals(null)) {
-
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        }
-
-        return returnedGuitar;
     }
 
 //    @GetMapping("/error")
